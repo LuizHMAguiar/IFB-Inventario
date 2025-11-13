46e100fe09ccd2ca311e9ffad1856ae4f61169c2
LuizHMAguiar <1397749@etfbsb.edu.br>
2025-11-13 12:13:23 -0300

    Configuração de permissão do servidor Render.com

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  preview: {
+    allowedHosts: ['ifb-inventario-1.onrender.com'], // ✅ adicione seu domínio aqui
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: '0.0.0.0',
+  },
 })
